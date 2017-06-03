2534d8a5f4d82cc5e0e218116b131248cef3f457
CratorCreator <jordanfischer13@gmail.com>
2017-06-03 14:49:58 +1000

    Getting there

---
+++ Assets/Scripts/DoorOpen.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpen : MonoBehaviour
+{
+    public GameObject door1, door2, door3, door4, door5, door6;
+
+    void Update()
+    {
+        StartCoroutine(open());
+    }
+
+    IEnumerator open()
+    {
+        if (Reticule.openDoor1 == true)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                door1.transform.position += new Vector3(0, 0, 1);
+                yield return new WaitForEndOfFrame();
+                Reticule.openDoor1 = false;
+
+                for (int e = 0; e < 1; e++)
+                {
+                    door2.transform.position += new Vector3(0, 0, -1);
+                    yield return new WaitForEndOfFrame();
+                    Reticule.openDoor1 = false;
+                }
+            }
+
+        }
+
+        if (Reticule.openDoor2 == true)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                door3.transform.position += new Vector3(-1, 0, 0);
+                yield return new WaitForEndOfFrame();
+                Reticule.openDoor2 = false;
+
+                for (int e = 0; e < 1; e++)
+                {
+                    door4.transform.position += new Vector3(1, 0, 0);
+                    yield return new WaitForEndOfFrame();
+                    Reticule.openDoor2 = false;
+                }
+            }
+
+        }
+
+        if (Reticule.openDoor3 == true)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                door5.transform.position += new Vector3(0, 0, 1);
+                yield return new WaitForEndOfFrame();
+                Reticule.openDoor3 = false;
+
+                for (int e = 0; e < 1; e++)
+                {
+                    door6.transform.position += new Vector3(0, 0, -1);
+                    yield return new WaitForEndOfFrame();
+                    Reticule.openDoor3 = false;
+                }
+            }
+
+        }
+    }
+}
---
+++ Assets/Scripts/Reticule.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Reticule : MonoBehaviour
+{
+
+    public static bool openDoor1, openDoor2, openDoor3;
+    public GameObject arm, pillGood, pillBad;
+    public Vector2 mousePos;
+    public float rayRange = 8;
+    public Animator animator;
+
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        mousePos = Input.mousePosition;
+        RaycastToMouse();
+        if (pillGood == null)
+        {
+            arm.SetActive(false);
+        }
+    }
+    void RaycastToMouse()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+
+
+        if (Input.GetKeyDown(KeyCode.E) && Physics.Raycast(ray, out hit, rayRange))
+        {
+
+            if (hit.collider.gameObject.tag == "GoodPills")
+            {
+                arm.SetActive(true);
+
+                animator.SetTrigger("Activate");
+                //SceneManager.LoadScene(2);
+                Destroy(pillGood, 1.2f);
+
+            }
+
+            if (hit.collider.gameObject.tag == "BadPills")
+            {
+                Destroy(hit.transform.gameObject);
+            }
+
+            if (hit.collider.gameObject.tag == "Door1")
+            {
+                openDoor1 = true;
+            }
+
+            if (hit.collider.gameObject.tag == "Door2")
+            {
+                openDoor2 = true;
+            }
+
+            if (hit.collider.gameObject.tag == "Door3")
+            {
+                openDoor3 = true;
+            }
+        }
+    }
+
+    void OnGUI()
+    {
+        GUI.Box(new Rect(mousePos.x, mousePos.y, 1, 1), "eh");
+    }
+}
